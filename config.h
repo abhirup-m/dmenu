6b000e482d2ae33ec0f976906a4b08286fe11fd7
SearyBlue <mukherjeeabhirup44@gmail.com>
2020-08-27 15:40:38 +0530

    changed colorcsheme to default and linehieght to 30

--- config.h
+++ config.h
@@ -10,13 +10,13 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                  fg         bg       */
-	[SchemeNorm]={ "#ffffff" ,"#222222"},
-	[SchemeSel] = { "#ffffff",  "#444444"},
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 40;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 30;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
