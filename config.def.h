1abc020bc7dc589211219fb655cfc63656365e4f
SearyBlue <mukherjeeabhirup44@gmail.com>
2022-03-26 09:53:43 +0530

    added border and center

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -27,4 +27,4 @@ static unsigned int lineheight = 30;         /* -h option; minimum height of a m
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 8;
+static unsigned int border_width = 0;
