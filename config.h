b36a6d837bcb0fd5da7a2dae1bb99ed7f5653203
SearyBlue <mukherjeeabhirup44@gmail.com>
2022-03-26 09:39:29 +0530

    changed font to roboto

--- config.h
+++ config.h
@@ -4,18 +4,18 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=14",
+	"RobotoMono Nerd Font:size=15:style=regular",
 //	"JoyPixels:pixelsize=15:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                  fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#ffffff", "#1d2021" },
+	[SchemeSel] = { "#000000", "#ff7700" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 30;
 static unsigned int lineheight = 30;         /* -h option; minimum height of a menu line     */
 
 /*
